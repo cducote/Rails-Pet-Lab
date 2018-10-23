3a037b5acffb01e2dd2fe812e729d52d2db7b350
Chris Ducote <chrisd@slappy.local>
2018-10-22 21:03:04 -0400

    8 out of 10 errors

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,5 +1,9 @@
 class Owner < ActiveRecord::Base
   # TODO: add validations
+  validates :first_name, presence: true, length: { maximum: 255 }
+  validates :last_name, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: true }
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
   before_save :normalize_phone_number
 
